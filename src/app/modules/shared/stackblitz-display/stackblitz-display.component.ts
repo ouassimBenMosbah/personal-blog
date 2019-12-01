a66e362a38ed9df49b2f3beb165008b84f34e205
Ouassim BEN MOSBAH <ouassim.benmosbah@gmail.com>
2019-12-01 16:38:40 +0100

    feat: improve stacblitz display component

--- src/app/modules/shared/stackblitz-display/stackblitz-display.component.ts
+++ src/app/modules/shared/stackblitz-display/stackblitz-display.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-stackblitz-display',
@@ -7,21 +7,80 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class StackblitzDisplayComponent implements OnInit {
   @Input() url: string;
+  @Input() file: string;
+  @Input() embed: boolean;
+  @Input() hideExplorer: boolean;
+  @Input() hideNavigation: boolean;
+  @Input() ctl: boolean;
+  @Input() view: 'editor' | 'preview';
+  @Input() hideDevTools: boolean;
+  @Input() devtoolsHeight: number;
+
+  private finalUrl: string;
   public stackblitzIframe: string;
   constructor() {}
 
   ngOnInit() {
+    this.setUrlFromInputs();
     this.stackblitzIframe = `
       <iframe
-        style="
-          width: 100%;
-          height: 100%;
-          min-height: 500px;
-          padding: 20px;
-          box-sizing: border-box;
-        "
-        src="${this.url}
+        class="stackblitz-iframe"
+        style="height: ${this.devtoolsHeight ? `${this.devtoolsHeight}px` : '100%'};"
+        src="${this.finalUrl}
       ">
       </iframe>`;
+  }
+
+  private setUrlFromInputs() {
+    if (!this.url) {
+      throw new Error('Missing url for stackblitz display');
+    }
+
+    if (this.devtoolsHeight && this.devtoolsHeight > 1000) {
+      this.devtoolsHeight = 1000;
+    }
+
+    const availableInputs = [
+      this._file,
+      this._embed,
+      this._hideExplorer,
+      this._hideNavigation,
+      this._ctl,
+      this._view,
+      this._hideDevTools
+    ].filter(inputs => !!inputs);
+
+    this.finalUrl = this.url;
+    if (availableInputs.length) {
+      this.finalUrl += `?${availableInputs.join('&')}`;
+    }
+  }
+
+  get _embed(): false | string {
+    return !!this.embed ? `embed=1` : false;
+  }
+
+  get _hideExplorer(): false | string {
+    return !!this.hideExplorer ? `hideExplorer=1` : false;
+  }
+
+  get _hideNavigation(): false | string {
+    return !!this.hideNavigation ? `hideNavigation=1` : false;
+  }
+
+  get _ctl(): false | string {
+    return !!this.ctl ? `ctl=1` : false;
+  }
+
+  get _hideDevTools(): false | string {
+    return !!this.hideDevTools ? `hideDevTools=1` : false;
+  }
+
+  get _view(): false | string {
+    return !!this.embed ? `embed=1` : false;
+  }
+
+  get _file(): false | string {
+    return !!this.file ? `file=${this.file}` : false;
   }
 }
